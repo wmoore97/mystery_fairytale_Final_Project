613ba082facd702cc8de64a47b9284c9f843e3b8
wmoore97 <45413596+wmoore97@users.noreply.github.com>
2019-11-20 18:16:17 -0500

    Added Correct Cleaned Mystery Folder
    Added a new folder with the correctly cleaned mystery books, as well as added the script used to clean it "mystery_cleaner"

--- cleaned data/mystery_cleaner.py
+++ cleaned data/mystery_cleaner.py
@@ -10,6 +10,8 @@ This script was designed to trim the header and footer info out of each ebook in
 
 import glob
 import nltk as nl
+import string
+from nltk.stem.snowball import SnowballStemmer
 
 #nl.download('stopwords')
 
@@ -22,13 +24,30 @@ for filename in glob.iglob(path+"/mystery/*.txt"):
     #iterate through each file in the folder, opening it
     with open(filename, encoding="utf8") as f:
         lines = f.read()
+        table = str.maketrans('', '', string.punctuation)
+        for punc in lines:
+            punc.translate(table)
         token_text = nl.tokenize.word_tokenize(lines)
         stop_words = set(nl.corpus.stopwords.words("english"))
+        stop_words.add('havent')
+        stop_words.add('could')
         filtered_data=[]
         for w in token_text:
             if w not in stop_words:
                 filtered_data.append(w)
+        filtered_data2 =[]
+        for word in filtered_data:
+            if word.isalpha():
+                filtered_data2.append(word)
+        filtered_data3=[]
+        for words in filtered_data2:
+            if len(words) > 1:
+                filtered_data3.append(words)
+        filtered_data4=[]
+        stemmer = SnowballStemmer("english")
+        for x in filtered_data3:
+            filtered_data4.append(stemmer.stem(x))
         f.close()
         o = open(filename, "w", encoding="utf8")
-        o.write(str(filtered_data))
+        o.write(str(filtered_data4))
         o.close()
